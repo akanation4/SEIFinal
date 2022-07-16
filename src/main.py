d2eca371c760e59965f2b2dd7dfbd689482b20b3
akanation4 <103402576+akanation4@users.noreply.github.com>
2022-07-16 15:18:46 +0900

    Add process to prepare data for machine learning

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 # ライブラリの読み込み
+from tkinter import Y
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
 # データの読み込み・加工
 df = pd.read_csv("../real_estate/chitose.csv", encoding="cp932")
@@ -23,9 +26,20 @@ df.loc[df['nearest_station_distance']=='2H?', ['nearest_station_distance']] = '1
 df["area"] = df["area"].astype(int)
 # 計算によって補完
 df.loc[df["transaction_price(unit_price_per_square_meter)"].isnull(),
-['transaction_price(unit_price_per_square_meter)']] = df['transaction_price'] / df['area']
+        ['transaction_price(unit_price_per_square_meter)']] = df['transaction_price'] / df['area']
 # ワンホットエンコーディング
 df_district = pd.get_dummies(df['district_name'], prefix='district_name')
-df = pd.concat([df, df_district], axis=1)
+df_type = pd.get_dummies(df['type'], prefix='type')
 # 欠損値の処理
 df.dropna(subset=['nearest_station_distance'], inplace=True)
+
+# 機械学習用のデータを作成
+df = pd.concat([df["area"], df["nearest_station_distance"],
+                df_district, df_type, df["transaction_price"]], axis=1)
+arr = df.values
+X = arr[:,:-1]
+Y = arr[:,-1]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+# 機械学習
+clf = MLPClassifier()
